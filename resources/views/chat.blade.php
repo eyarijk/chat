ef596e6486f1ccddf5028bcb8f8faf549a40d1eb
eyarij <eyarij@gmail.com>
2017-11-12 00:09:22 +0200

    error

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,100 @@
+@extends('layouts.main')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel panel-default">
+                <div class="panel-heading"><div class="text-center">Чат</div></div>
+
+                <div class="panel-body" style="height: 340px; overflow: auto;">
+                    <div class="chat-body" style="margin-top: 10px;">
+                        <div class="header">
+                            <strong> Mr.Evrey </strong>
+                            <small class="pull-right text-muted"><span class="glyphicon glyphicon-time"></span> 2 min ago</small>
+                        </div>
+                        <div class="chatcolor">
+                            Hello, my name Mr.Evrey
+                        </div>
+                    </div>
+                    <div class="chat-body" style="margin-top: 10px;">
+                        <div class="header">
+                            <strong> Mr.Evrey </strong>
+                            <small class="pull-right text-muted"><span class="glyphicon glyphicon-time"></span> 2 min ago</small>
+                        </div>
+                        <div class="chatcolor">
+                            Hello, my name Mr.Evrey
+                        </div>
+                    </div>
+                    <div class="chat-body" style="margin-top: 10px;">
+                        <div class="header">
+                            <strong> Mr.Evrey </strong>
+                            <small class="pull-right text-muted"><span class="glyphicon glyphicon-time"></span> 2 min ago</small>
+                        </div>
+                        <div class="chatcolor">
+                            Hello, my name Mr.Evrey
+                        </div>
+                    </div>
+                    <div class="chat-body" style="margin-top: 10px;">
+                        <div class="header">
+                            <strong> Mr.Evrey </strong>
+                            <small class="pull-right text-muted"><span class="glyphicon glyphicon-time"></span> 2 min ago</small>
+                        </div>
+                        <div class="chatcolor">
+                            Hello, my name Mr.Evrey
+                        </div>
+                    </div>
+                    <div class="chat-body" style="margin-top: 10px;">
+                        <div class="header">
+                            <strong> Mr.Evrey </strong>
+                            <small class="pull-right text-muted"><span class="glyphicon glyphicon-time"></span> 2 min ago</small>
+                        </div>
+                        <div class="chatcolor">
+                            Hello, my name Mr.Evrey
+                        </div>
+                    </div>
+                    <div class="chat-body" style="margin-top: 10px;">
+                        <div class="header">
+                            <strong> Mr.Evrey </strong>
+                            <small class="pull-right text-muted"><span class="glyphicon glyphicon-time"></span> 2 min ago</small>
+                        </div>
+                        <div class="chatcolor">
+                            Hello, my name Mr.Evrey
+                        </div>
+                    </div>
+                    <div class="chat-body" style="margin-top: 10px;">
+                        <div class="header">
+                            <strong> Mr.Evrey </strong>
+                            <small class="pull-right text-muted"><span class="glyphicon glyphicon-time"></span> 2 min ago</small>
+                        </div>
+                        <div class="chatcolor">
+                            Hello, my name Mr.Evrey
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div class="panel-footer">
+                <div class="input-group">
+                    <input type="text" class="form-control" name="" placeholder="Написати повідомлення...">
+                    <span class="input-group-btn">
+                        <button class="btn btn-warning">Відправити</button>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+<style type="text/css">
+    .chatcolor{
+        color: #fff;
+        background-color: #f0ad4e;
+        padding: 10px;
+        border-bottom-left-radius: 24px;
+        border-bottom-right-radius: 28px;
+        border-top-right-radius: 28px;
+        margin-top: 6px;
+    }
+</style>
